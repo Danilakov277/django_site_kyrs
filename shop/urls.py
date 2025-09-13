1fdf272f1d73ad9f00004dc039124c2bfc915bc5
Danila <Danilakov2006@gmail.com>
2025-09-14 02:59:16 +0300

    add review

--- shop/urls.py
+++ shop/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('reviews/', include('review.urls', namespace='review')),
     path('', include('main.urls', namespace='main')),
 ]
 
