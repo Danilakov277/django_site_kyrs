e6024f2e1634f14950f4f2443ecc7bba53513616
Danila <Danilakov2006@gmail.com>
2025-09-16 01:31:03 +0300

    add custom user

--- shop/settings.py
+++ shop/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d(!y!#611j18c$611lu7l(m6ps0q4x)b0lie@y#=px1(pdea*i'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,10 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'user',
     'main',
     'review',
-    'user',
+
 ]
 
 MIDDLEWARE = [
@@ -128,3 +132,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'user.CustomUser'
--- user/models.py
+++ user/models.py
@@ -1,13 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, \
+BaseUserManager
+from django.utils.html import strip_tags
 
-class user(models.Model):
-    name=models.CharField(max_length=100, db_index=True)
-    email = models.EmailField()
-    is_trener = models.BooleanField()
-  
-    class Meta:
-        ordering = ('name',)
-        verbose_name = 'пользователь'
-        verbose_name_plural = 'пользователи'
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, first_name,last_name,password=None,**extra_fields):
+        if not email:
+            return ValueError('The email fild must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email,first_name=first_name,last_name=last_name,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+    
+
+    def create_superuser(self, email, first_name,last_name,password=None,**extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('super_user must have is_staff True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('super_user must have is_superuser True')
+        return self.create_user(email,first_name,last_name,password,**extra_fields)
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True, max_length=66)
+    first_name=models.CharField(max_length=66)
+    last_name = models.CharField(max_length=66)
+    is_trener = models.BooleanField(default=False)
+
+    username = models.CharField(max_length=150, unique=True,null=True,blank=True)
+
+
+    objects = CustomUserManager()
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
     def __str__(self):
-        return self.name
+        return self.email
